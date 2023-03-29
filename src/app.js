8540efc5d1c845064da9867b1f32ad8a93e0f780
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-03-29 16:53:46 +0300

    custom errors implementation and error handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Express from 'express';
+import 'express-async-errors';
 
 import authRouter from './routes/authRouter.js';
 import usersRouter from './routes/usersRouter.js';
 
-import notFoundMiddleware from './middlewares/not-found.js';
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 const app = Express();
 
@@ -14,5 +16,6 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 export default app;
