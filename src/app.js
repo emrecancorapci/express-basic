d414934288dca4ba32d6d6e751d64a892dd4a9da
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-03-29 16:59:40 +0300

    helmet, cors, xss, rate limiter implementation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import Express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
 import 'express-async-errors';
 
 import authRouter from './routes/authRouter.js';
@@ -9,6 +13,18 @@ import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
 const app = Express();
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limits each IP to 100 request per windowMs
+  })
+);
+app.use(Express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use(Express.json());
 app.use(Express.static('./public'));
